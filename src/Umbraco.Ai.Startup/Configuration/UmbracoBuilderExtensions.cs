c16d8cbdc45799d9c6b0a6d5494c213f6c003eac
mattbrailsford <me@mattbrailsford.com>
2025-11-26 19:12:58 +0000

    Wire up persistence layer

--- src/Umbraco.Ai.Startup/Configuration/UmbracoBuilderExtensions.cs
+++ src/Umbraco.Ai.Startup/Configuration/UmbracoBuilderExtensions.cs
@@ -1,19 +1,29 @@
 using Umbraco.Ai.Core.Registry;
+using Umbraco.Ai.Persistence.Extensions;
 using Umbraco.Cms.Core.DependencyInjection;
 
 namespace Umbraco.Ai.Extensions;
 
+/// <summary>
+/// Provides extension methods for configuring Umbraco AI services on the Umbraco builder.
+/// </summary>
 public static class UmbracoBuilderExtensions
 {
+    /// <summary>
+    /// Adds Umbraco AI services to the Umbraco builder.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
     public static IUmbracoBuilder AddUmbracoAi(this IUmbracoBuilder builder)
     {
         // Prevent multiple registrations
         if (builder.Services.Any(x => x.ServiceType == typeof(IAiRegistry)))
             return builder;
-        
+
         builder.AddUmbracoAiCore();
+        builder.AddUmbracoAiPersistence();
         builder.AddUmbracoAiWeb();
-        
+
         return builder;
     }
 }

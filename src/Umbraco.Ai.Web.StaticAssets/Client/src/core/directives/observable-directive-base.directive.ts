1e46c3926444621b7b9eb173c266d94c9a0d6dd2
mattbrailsford <me@mattbrailsford.com>
2025-11-29 11:48:30 +0000

    Implemented WithProvider directive

---
+++ src/Umbraco.Ai.Web.StaticAssets/Client/src/core/directives/observable-directive-base.directive.ts
@@ -0,0 +1,49 @@
+
+import { noChange } from '@umbraco-cms/backoffice/external/lit';
+import { AsyncDirective } from 'lit/async-directive.js';
+import type { Observable, Subscription } from 'rxjs';
+
+export abstract class UaiObserveDirectiveBase<T> extends AsyncDirective {
+
+    observable: Observable<T | undefined> | undefined;
+    subscription: Subscription | undefined;
+
+    formatValue: (val: T) => unknown = (v) => v;
+
+    // When the observable changes, unsubscribe to the old one and
+    // subscribe to the new one
+    renderObservable(observable: Observable<T | undefined>) {
+        if (this.observable !== observable) {
+            this.subscription?.unsubscribe();
+            this.observable = observable;
+            if (this.isConnected) {
+                this.subscribe(observable);
+            }
+        }
+        return noChange;
+    }
+
+    // Subscribes to the observable, calling the directive's asynchronous
+    // setValue API each time the value changes
+    subscribe(observable: Observable<T | undefined>) {
+        this.subscription = observable.subscribe((v: T | undefined) => {
+            if (v) {
+                this.setValue(this.formatValue(v));
+            }
+        });
+    }
+
+    // When the directive is disconnected from the DOM, unsubscribe to ensure
+    // the directive instance can be garbage collected
+    disconnected() {
+        this.subscription?.unsubscribe();
+    }
+
+    // If the subtree the directive is in was disconnected and subsequently
+    // re-connected, re-subscribe to make the directive operable again
+    reconnected() {
+        if (this.observable) {
+            this.subscribe(this.observable);
+        }
+    }
+}

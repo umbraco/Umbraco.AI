bf2590ad7f609cc03315f39896d3b36644d52ee2
mattbrailsford <me@mattbrailsford.com>
2025-11-29 11:48:30 +0000

    Implemented WithProvider directive

---
+++ src/Umbraco.Ai.Web.StaticAssets/Client/src/provider/directives/with-provider.directive.ts
@@ -0,0 +1,17 @@
+import { UmbControllerHost } from "@umbraco-cms/backoffice/controller-api";
+import { directive } from "@umbraco-cms/backoffice/external/lit";
+import { UaiProviderDetailRepository } from "../repository";
+import { UaiWithEntityBaseDirective } from "../../core/directives/with-entity-directive-base.directive.ts";
+import type { UaiProviderDetailModel } from "../types.ts";
+
+export class WithProviderDirective extends UaiWithEntityBaseDirective<UaiProviderDetailModel> {
+    async getEntity(host: UmbControllerHost, unique: string)
+    {
+        const repository = new UaiProviderDetailRepository(host);
+        return repository.requestById(unique).then(({ data:provider }) => {
+            return provider;
+        });
+    }
+}
+
+export const uaiWithProvider = directive(WithProviderDirective);

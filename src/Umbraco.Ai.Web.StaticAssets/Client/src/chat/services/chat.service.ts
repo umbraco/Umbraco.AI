40dc3ab5027336279f5b19b73173d35436e1cac2
mattbrailsford <me@mattbrailsford.com>
2025-11-30 10:09:25 +0000

    Setup API build

---
+++ src/Umbraco.Ai.Web.StaticAssets/Client/src/chat/services/chat.service.ts
@@ -0,0 +1,9 @@
+import { UmbControllerBase } from "@umbraco-cms/backoffice/class-api";
+import { UmbControllerHost } from "@umbraco-cms/backoffice/controller-api";
+
+/** @public **/
+export class UaiChatService extends UmbControllerBase {
+    constructor(host: UmbControllerHost) {
+        super(host);
+    }
+}
---
+++ src/Umbraco.Ai.Web.StaticAssets/Client/src/exports.ts
@@ -0,0 +1,1 @@
+export * from './chat/exports.js';

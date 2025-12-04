eba416685b1f0a55f28e10c863d6466d2a2ea91d
mattbrailsford <me@mattbrailsford.com>
2025-12-04 11:14:54 +0000

    Export empty guid

--- src/Umbraco.Ai.Web.StaticAssets/Client/src/core/exports.ts
+++ src/Umbraco.Ai.Web.StaticAssets/Client/src/core/exports.ts
@@ -1,3 +1,4 @@
 export { UaiProfilePickerElement } from './components/profile-picker/profile-picker.element.js';
 export * from './events/exports.js';
 export * from './command/exports.js';
+export { UAI_EMPTY_GUID } from './index.js';

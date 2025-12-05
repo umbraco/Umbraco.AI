a683a970b31d37fe387e716c5bbcd6eb1589ef3c
mattbrailsford <me@mattbrailsford.com>
2025-12-05 13:32:22 +0000

    Make UAI_EMPTY_GUID public

--- src/Umbraco.Ai.Web.StaticAssets/Client/src/core/index.ts
+++ src/Umbraco.Ai.Web.StaticAssets/Client/src/core/index.ts
@@ -2,4 +2,8 @@ export * from "./command/index.js";
 export * from './components/index.js';
 export * from './events/index.js';
 
+/**
+ * A constant representing an empty GUID.
+ * @public
+ */
 export const UAI_EMPTY_GUID = "00000000-0000-0000-0000-000000000000";
